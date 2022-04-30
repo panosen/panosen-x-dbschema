f5c66894751401da6d5b71bde37fb9784b03cbc3
harriszhang <harriszhang@live.cn>
2022-04-30 20:10:30 +0800

    update dependency

--- src/main/java/com/panosen/dbschema/information_schema/Column.java
+++ src/main/java/com/panosen/dbschema/information_schema/Column.java
@@ -186,5 +186,179 @@ public class Column {
     @Type(type = Types.VARCHAR)
     private String srsId;
 
+    public String getTableCatalog() {
+        return tableCatalog;
+    }
 
+    public void setTableCatalog(String tableCatalog) {
+        this.tableCatalog = tableCatalog;
+    }
+
+    public String getTableSchema() {
+        return tableSchema;
+    }
+
+    public void setTableSchema(String tableSchema) {
+        this.tableSchema = tableSchema;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public long getOrdinalPosition() {
+        return ordinalPosition;
+    }
+
+    public void setOrdinalPosition(long ordinalPosition) {
+        this.ordinalPosition = ordinalPosition;
+    }
+
+    public String getColumnDefault() {
+        return columnDefault;
+    }
+
+    public void setColumnDefault(String columnDefault) {
+        this.columnDefault = columnDefault;
+    }
+
+    public String getIsNullable() {
+        return isNullable;
+    }
+
+    public void setIsNullable(String isNullable) {
+        this.isNullable = isNullable;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public Long getCharacterMaximumLength() {
+        return characterMaximumLength;
+    }
+
+    public void setCharacterMaximumLength(Long characterMaximumLength) {
+        this.characterMaximumLength = characterMaximumLength;
+    }
+
+    public Long getCharacterOctetLength() {
+        return characterOctetLength;
+    }
+
+    public void setCharacterOctetLength(Long characterOctetLength) {
+        this.characterOctetLength = characterOctetLength;
+    }
+
+    public Long getNumericPrecision() {
+        return numericPrecision;
+    }
+
+    public void setNumericPrecision(Long numericPrecision) {
+        this.numericPrecision = numericPrecision;
+    }
+
+    public Long getNumericScale() {
+        return numericScale;
+    }
+
+    public void setNumericScale(Long numericScale) {
+        this.numericScale = numericScale;
+    }
+
+    public Long getDatetimePrecision() {
+        return datetimePrecision;
+    }
+
+    public void setDatetimePrecision(Long datetimePrecision) {
+        this.datetimePrecision = datetimePrecision;
+    }
+
+    public String getCharacterSetName() {
+        return characterSetName;
+    }
+
+    public void setCharacterSetName(String characterSetName) {
+        this.characterSetName = characterSetName;
+    }
+
+    public String getCollationName() {
+        return collationName;
+    }
+
+    public void setCollationName(String collationName) {
+        this.collationName = collationName;
+    }
+
+    public String getColumnType() {
+        return columnType;
+    }
+
+    public void setColumnType(String columnType) {
+        this.columnType = columnType;
+    }
+
+    public String getColumnKey() {
+        return columnKey;
+    }
+
+    public void setColumnKey(String columnKey) {
+        this.columnKey = columnKey;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+
+    public String getPrivileges() {
+        return privileges;
+    }
+
+    public void setPrivileges(String privileges) {
+        this.privileges = privileges;
+    }
+
+    public String getColumnComment() {
+        return columnComment;
+    }
+
+    public void setColumnComment(String columnComment) {
+        this.columnComment = columnComment;
+    }
+
+    public String getGenerationExpression() {
+        return generationExpression;
+    }
+
+    public void setGenerationExpression(String generationExpression) {
+        this.generationExpression = generationExpression;
+    }
+
+    public String getSrsId() {
+        return srsId;
+    }
+
+    public void setSrsId(String srsId) {
+        this.srsId = srsId;
+    }
 }
